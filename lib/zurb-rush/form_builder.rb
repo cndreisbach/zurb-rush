b46238d17561a0066cc4cd2a81b082e68a71ac80
Clinton R. Nixon <crnixon@gmail.com>
2012-08-17 13:36:35 -0400

    Add time zone support

--- lib/zurb-rush/form_builder.rb
+++ lib/zurb-rush/form_builder.rb
@@ -1,12 +1,13 @@
 module ZurbRush
   class FormBuilder < ActionView::Helpers::FormBuilder
-    FIELD_MATCHERS = {
+    FIELD_NAME_MATCHERS = {
       /password/ => :password,
+      /search/ => :search,
       /email/ => :email,
       /phone/ => :phone,
-      /fax/ => :phone
+      /fax/ => :phone,
       /url/ => :url,
-      String => :text
+      /time_zone/ => :time_zone
     }
 
     def input(field_name, options = {})
@@ -18,9 +19,14 @@ module ZurbRush
       field_name = field_name.to_s
 
       field_type = options.delete(:as)
-      field_type ||= FIELD_MATCHERS.to_a.select { |matcher, type| matcher === field_name }.map { |_, type| type }.first
+      field_type ||= FIELD_NAME_MATCHERS.to_a.select { |matcher, type| matcher === field_name }.map { |_, type| type }.first
+      field_type ||= :text
 
       send("#{field_type}_field", field_name, options)
+    end
+
+    def time_zone_field(field_name, options)
+      time_zone_select(field_name, options)
     end
   end
 end
